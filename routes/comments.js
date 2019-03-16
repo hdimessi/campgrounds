876d459a313deea1aac9c8cf2565cb42b73f192f
hdimessi <hussein.dimessi@gmail.com>
2019-03-16 08:54:04 +0100

    add comment permissions

--- routes/comments.js
+++ routes/comments.js
@@ -37,7 +37,7 @@ router.post("/", isLoggedIn, function(req, res) {
   });
 });
 
-router.get("/:comment_id/edit", function (req, res) {
+router.get("/:comment_id/edit", verifyCommentOwnership, function (req, res) {
   Comment.findById(req.params.comment_id, function (err, foundComment) {
     if (err) {
       res.redirect("back")
@@ -52,7 +52,7 @@ router.get("/:comment_id/edit", function (req, res) {
   })
 })
 
-router.put("/:comment_id", function (req, res) {
+router.put("/:comment_id", verifyCommentOwnership, function (req, res) {
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (err, updatedComment) {
     if (err) {
       res.redirect("/campgrounds")
@@ -62,7 +62,7 @@ router.put("/:comment_id", function (req, res) {
   })
 })
 
-router.delete("/:comment_id", function (req, res) {
+router.delete("/:comment_id", verifyCommentOwnership, function (req, res) {
   Comment.findByIdAndDelete(req.params.comment_id, function (err) {
     if (err) {
       res.redirect("/campgrounds")
@@ -77,6 +77,24 @@ function isLoggedIn(req, res, next) {
       return next();
     }
     res.redirect("/campgrounds");
+}
+
+function verifyCommentOwnership(req, res, next) {
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
+      if (err) {
+        res.redirect("back");
+      } else {
+        if (foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
 }
 
 module.exports = router;
